71f6e4b1066ff4954c8aeb59d62f2fc5bf677d30
Dehemi-Perera <lilyvihara07@gmail.com>
2025-09-20 21:39:28 +0530

    port change

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     
     'https://my-projectfront.vercel.app',              // Your frontend Vercel URL
     'http://localhost:3000',                           // Local development frontend
-    'http://localhost:3001',                           // Alternative local port
+    'http://localhost:3500',                           // Alternative local port
 ]
 
 module.exports = allowedOrigins
