51a72f5db6993e9e29eb49e66c49c3f5d58bb202
Dehemi-Perera <lilyvihara07@gmail.com>
2025-09-20 22:44:44 +0530

    alters

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     
-    'https://my-projectfront.vercel.app',              // Your frontend Vercel URL
-    'http://localhost:3000',                           // Local development frontend
+    'https://my-projectfront.vercel.app/',              // Your frontend Vercel URL
+                              // Local development frontend
     'http://localhost:3500',                           // Alternative local port
 ]
 
